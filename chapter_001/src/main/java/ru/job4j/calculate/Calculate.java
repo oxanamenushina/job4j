7e7bac6252faf675c4fb358051c245e8ac4422a6
oxanamenushina <oxsm@mail.ru>
2018-11-21 19:28:20 +0300

    1.3. JavaDoc. Code convention[#89345]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
-public class Calculate {
+/**
+* Calculate.
+*
+* @author Oxana Menushina (oxsm@mail.ru)
+*/
+public class Calculate {	
+	/**
+	* Method main.
+	* @param args - args.
+	*/
 	public static void main(String [] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Oxana Menushina (oxsm@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
