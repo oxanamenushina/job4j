c6bc870e0a167a8f2b4530a8f72d25bc45b66b5c
oxanamenushina <oxsm@mail.ru>
2018-11-23 15:06:48 +0300

    3.1. Элементарный калькулятор.[#89336]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Oxana Menushina (oxsm@mail.ru)
+ */
+public class Calculate {
+    /**
+     * Method main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+}
