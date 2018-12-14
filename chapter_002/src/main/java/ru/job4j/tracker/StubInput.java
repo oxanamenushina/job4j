5cfe2b056324c56cab047e7a4e26d4987110f7ff
oxanamenushina <oxsm@mail.ru>
2018-12-15 00:01:58 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#89384]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -0,0 +1,25 @@
+package ru.job4j.tracker;
+
+/**
+ * @version $Id$
+ * @since 0.1
+ */
+public class StubInput implements Input {
+
+    private final String[] value;
+    private int position;
+
+    public StubInput(final String[] value) {
+        this.value = value;
+    }
+
+    /**
+     * Метод заменяет ответы пользователя.
+     * @param question.
+     * Ответ пользователя.
+     */
+    @Override
+    public String ask(String question) {
+        return this.value[this.position++];
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,60 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+/**
+ * Test StartUI.
+ */
+public class StartUITest {
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerNotUpdateValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", "неправильный ID", "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+    }
+
+    @Test
+    public void whenDeleteThenTrackerDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item first = tracker.add(new Item("test name1", "desc1"));
+        Item second = tracker.add(new Item("test name2", "desc2"));
+        Input input = new StubInput(new String[]{"3", first.getId(), "6"});
+        Item[] expect = {second};
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll(), is(expect));
+    }
+
+    @Test
+    public void whenDeleteThenTrackerNotDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item first = tracker.add(new Item("test name1", "desc1"));
+        Item second = tracker.add(new Item("test name2", "desc2"));
+        Input input = new StubInput(new String[]{"3", "неправильный ID", "6"});
+        Item[] expect = {first, second};
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll(), is(expect));
+    }
+
+}
