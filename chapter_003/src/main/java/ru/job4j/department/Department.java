1486feac4e77499485f04e83e18f78daba43480e
oxanamenushina <oxsm@mail.ru>
2019-03-29 08:59:22 +0300

    Отсортировать департаменты [#34352]

--- chapter_003/src/main/java/ru/job4j/department/Department.java
+++ chapter_003/src/main/java/ru/job4j/department/Department.java
@@ -1,7 +1,6 @@
 package ru.job4j.department;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 /**
  * @author Oxana Menushina (oxsm@mail.ru).
@@ -10,16 +9,47 @@ import java.util.Comparator;
  */
 public class Department {
     /**
+     * Метод добавляет пропущенные подразделения.
+     * @param subdivisions массив кодов подразделений
+     * @return массив кодов подразделений c добавленными строками с кодом верхнеуровневых подразделений
+     */
+    public String[] addMissed(String[] subdivisions) {
+        Set<String> codes = new HashSet<>(Arrays.asList(subdivisions));
+        for (String subdiv : subdivisions) {
+            String code = subdiv;
+            while (code.contains("\\")) {
+                code = code.substring(0, code.lastIndexOf("\\"));
+                codes.add(code);
+            }
+        }
+        return codes.toArray(new String[0]);
+    }
+
+    /**
+     * Метод добавляет пропущенные подразделения.
+     * @param subdivisions массив кодов подразделений
+     * @return Map, ключами которой являются коды подразделений, значениями - массивы их составных частей
+     */
+    private Map<String, String[]> codesToMap(String[] subdivisions) {
+        Map<String, String[]> codes = new HashMap<>();
+        for (String subdiv : this.addMissed(subdivisions)) {
+            codes.put(subdiv, subdiv.split("\\\\"));
+        }
+        return codes;
+    }
+
+    /**
      * Метод сортирует массив кодов подразделений по возрастанию.
-     * @param codes массив кодов подразделений
+     * @param subdivisions массив кодов подразделений
      * @return упорядоченный по возрастанию массив кодов подразделений
      */
-    public String[] ascendingSort(String[] codes) {
-        Arrays.sort(codes, new Comparator<String>() {
-            public int compare(String subdivision1, String subdivision2) {
+    public String[] ascendingSort(String[] subdivisions) {
+        String[] total = this.addMissed(subdivisions);
+        Arrays.sort(total, new Comparator<String>() {
+            public int compare(String subdiv1, String subdiv2) {
                 int result = 0;
-                String[] codes1 = subdivision1.split("\\\\");
-                String[] codes2 = subdivision2.split("\\\\");
+                String[] codes1 = codesToMap(subdivisions).get(subdiv1);
+                String[] codes2 = codesToMap(subdivisions).get(subdiv2);
                 for (int i = 0; i < (codes1.length > codes2.length ? codes2.length : codes1.length); i++) {
                     result = codes1[i].length() == codes2[i].length()
                             ? codes1[i].compareTo(codes2[i]) : codes1[i].length() - codes2[i].length();
@@ -30,20 +60,21 @@ public class Department {
                 return result != 0 ? result : codes1.length != codes2.length ? codes1.length - codes2.length : 0;
             }
         });
-        return codes;
+        return total;
     }
 
     /**
      * Метод сортирует массив кодов подразделений по убыванию.
-     * @param codes массив кодов подразделений
+     * @param subdivisions массив кодов подразделений
      * @return упорядоченный по убыванию массив кодов подразделений
      */
-    public String[] descendingSort(String[] codes) {
-        Arrays.sort(codes, new Comparator<String>() {
-            public int compare(String subdivision1, String subdivision2) {
+    public String[] descendingSort(String[] subdivisions) {
+        String[] total = this.addMissed(subdivisions);
+        Arrays.sort(total, new Comparator<String>() {
+            public int compare(String subdiv1, String subdiv2) {
                 int result = 0;
-                String[] codes1 = subdivision1.split("\\\\");
-                String[] codes2 = subdivision2.split("\\\\");
+                String[] codes1 = codesToMap(subdivisions).get(subdiv1);
+                String[] codes2 = codesToMap(subdivisions).get(subdiv2);
                 for (int i = 0; i < (codes1.length > codes2.length ? codes2.length : codes1.length); i++) {
                     result = codes1[i].length() == codes2[i].length()
                             ? codes2[i].compareTo(codes1[i]) : codes2[i].length() - codes1[i].length();
@@ -54,6 +85,6 @@ public class Department {
                 return result != 0 ? result : codes1.length != codes2.length ? codes1.length - codes2.length : 0;
             }
         });
-        return codes;
+        return total;
     }
 }
--- chapter_003/src/test/java/ru/job4j/department/DepartmentTest.java
+++ chapter_003/src/test/java/ru/job4j/department/DepartmentTest.java
@@ -3,6 +3,7 @@ package ru.job4j.department;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
 import static org.hamcrest.core.Is.is;
 
 /**
@@ -12,20 +13,68 @@ import static org.hamcrest.core.Is.is;
  */
 public class DepartmentTest {
     @Test
-    public void whenAscendingSort() {
+    public void whenOneAddMissedThenThree() {
         Department department = new Department();
-        assertThat(department.ascendingSort(new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K2\\SK1",
-                        "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2", "K1"}),
+        assertThat(department.addMissed(new String[] {"K1\\SK1\\SSK1"}),
+                arrayContainingInAnyOrder("K1", "K1\\SK1", "K1\\SK1\\SSK1")
+        );
+    }
+
+    @Test
+    public void whenTwoAddMissedThenSix() {
+        Department department = new Department();
+        assertThat(department.addMissed(new String[] {"K1\\SK1\\SSK1", "K2\\SK1\\SSK2"}),
+                arrayContainingInAnyOrder("K1", "K1\\SK1", "K1\\SK1\\SSK1", "K2", "K2\\SK1", "K2\\SK1\\SSK2")
+        );
+    }
+
+    @Test
+    public void whenOneAscendingSortThenThreeSorted() {
+        Department department = new Department();
+        assertThat(department.ascendingSort(new String[] {"K1\\SK1\\SSK2"}),
+                is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK2"})
+        );
+    }
+
+    @Test
+    public void whenTwoAscendingSortThenFiveSorted() {
+        Department department = new Department();
+        assertThat(department.ascendingSort(new String[] {"K1\\SK1\\SSK1", "K2\\SK1"}),
+                is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K2", "K2\\SK1"})
+        );
+    }
+
+    @Test
+    public void whenSevenAscendingSortThenNineSorted() {
+        Department department = new Department();
+        assertThat(department.ascendingSort(new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1",
+                        "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"}),
                 is(new String[] {"K1", "K1\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K1\\SK2",
                         "K2", "K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"})
         );
     }
 
     @Test
-    public void whenDescendingSort() {
+    public void whenTwoDescendingSortThenThreeSorted() {
         Department department = new Department();
-        assertThat(department.descendingSort(new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K2\\SK1",
-                        "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2", "K1"}),
+        assertThat(department.descendingSort(new String[] {"K1\\SK1", "K2"}),
+                is(new String[] {"K2", "K1", "K1\\SK1"})
+        );
+    }
+
+    @Test
+    public void whenTwoDescendingSortThenFiveSorted() {
+        Department department = new Department();
+        assertThat(department.descendingSort(new String[] {"K1\\SK1\\SSK1", "K2\\SK1"}),
+                is(new String[] {"K2", "K2\\SK1", "K1", "K1\\SK1", "K1\\SK1\\SSK1"})
+        );
+    }
+
+    @Test
+    public void whenSevenDescendingSortThenNineSorted() {
+        Department department = new Department();
+        assertThat(department.descendingSort(new String[] {"K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1",
+                        "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"}),
                 is(new String[] {"K2", "K2\\SK1", "K2\\SK1\\SSK2", "K2\\SK1\\SSK1", "K1", "K1\\SK2",
                         "K1\\SK1", "K1\\SK1\\SSK2", "K1\\SK1\\SSK1"})
         );
