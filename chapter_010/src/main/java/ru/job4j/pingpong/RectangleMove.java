8f86ab72b7d64ce35b5749f138fa7570478b3d5f
oxanamenushina <oxsm@mail.ru>
2020-01-27 21:53:48 +0300

    1. Пин-понг.[#226397]

---
+++ chapter_010/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,63 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ * RectangleMove.
+ *
+ * @author Oxana Menushina (oxsm@mail.ru).
+ * @version $Id$
+ * @since 0.1
+ */
+public class RectangleMove implements Runnable {
+
+    /**
+     * Rectangle.
+     */
+    private final Rectangle rect;
+
+    /**
+     * The size of the field on the X axis.
+     */
+    private final int limX;
+
+    /**
+     * The size of the field on the Y axis.
+     */
+    private final int limY;
+
+    /**
+     * Rectangle directions along the X and Y axes.
+     */
+    private int[] direction = {1, 1};
+
+    public RectangleMove(Rectangle rect, int limX, int limY) {
+        this.rect = rect;
+        this.limX = limX;
+        this.limY = limY;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            this.setDirection();
+            this.rect.setX(this.rect.getX() + this.direction[0]);
+            this.rect.setY(this.rect.getY() + this.direction[1]);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * The method sets the direction to the rectangle.
+     */
+    private void setDirection() {
+        this.direction[0] = this.rect.getX() == 0 ? 1 : this.rect.getX() + this.rect.getWidth() == this.limX ? -1
+                : this.direction[0];
+        this.direction[1] =  this.rect.getY() == 0 ? 1 : this.rect.getY() + this.rect.getHeight() == this.limY ? -1
+                : this.direction[1];
+    }
+}
