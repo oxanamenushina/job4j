7cc4554ba75ce2c3bfe388506fc53aaea7c0d01c
oxanamenushina <oxsm@mail.ru>
2020-02-03 06:45:42 +0300

    1. Реализовать шаблон Producer Consumer.[#226393]

---
+++ chapter_010/src/main/java/ru/job4j/blockingqueue/SimpleBlockingQueue.java
@@ -0,0 +1,60 @@
+package ru.job4j.blockingqueue;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * SimpleBlockingQueue.
+ *
+ * @author Oxana Menushina (oxsm@mail.ru).
+ * @version $Id$
+ * @since 0.1
+ */
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    /**
+     * The queue.
+     */
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    /**
+     * The method adds the value to the queue.
+     * @param value the value to add.
+     */
+    public synchronized void offer(T value) {
+        while (!this.queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        this.queue.offer(value);
+        System.out.println("Producer: " + value.toString());
+        notifyAll();
+    }
+
+    /**
+     * The method returns and deletes
+     * the value from the the queue.
+     * @return value.
+     */
+    public synchronized T poll() {
+        while (this.queue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        T value = this.queue.poll();
+        System.out.println("Consumer: " + value.toString());
+        notifyAll();
+        return value;
+    }
+}
---
+++ chapter_010/src/test/java/ru/job4j/blockingqueue/SimpleBlockingQueueTest.java
@@ -0,0 +1,86 @@
+package ru.job4j.blockingqueue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * SimpleBlockingQueueTest.
+ *
+ * @author Oxana Menushina (oxsm@mail.ru).
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleBlockingQueueTest {
+
+    private final ByteArrayOutputStream mem = new ByteArrayOutputStream();
+    private final PrintStream out = System.out;
+
+    @Before
+    public void loadMem() {
+        System.setOut(new PrintStream(this.mem));
+    }
+
+    @After
+    public void loadSys() {
+        System.setOut(this.out);
+    }
+
+    /**
+     * Producer.
+     */
+    public class Producer implements Runnable {
+
+        private SimpleBlockingQueue<Integer> queue;
+        private int count;
+
+        public Producer(SimpleBlockingQueue<Integer> queue, int count) {
+            this.queue = queue;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            IntStream.range(1, this.count + 1).forEach(i -> this.queue.offer(i));
+        }
+    }
+
+    /**
+     * Consumer.
+     */
+    public class Consumer implements Runnable {
+
+        private SimpleBlockingQueue<Integer> queue;
+        private int count;
+
+        public Consumer(SimpleBlockingQueue<Integer> queue, int count) {
+            this.queue = queue;
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            IntStream.range(0, this.count).forEach(i -> this.queue.poll());
+        }
+    }
+
+    @Test
+    public void whenProducerAdd5ElementsThenConsumerGet5Elements() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(new Producer(queue, 5));
+        Thread consumer = new Thread(new Consumer(queue, 5));
+        consumer.start();
+        producer.start();
+        producer.join();
+        consumer.join();
+        assertThat(this.mem.toString(), is(String.format("Producer: 1%nConsumer: 1%nProducer: 2%nConsumer: 2%n"
+                + "Producer: 3%nConsumer: 3%nProducer: 4%nConsumer: 4%nProducer: 5%nConsumer: 5%n")));
+    }
+}
